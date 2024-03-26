2e3e4b25d32d06d503ffae217970bb697c3955df
SARATH KUMAR K <sarathkrishnan2332@gmail.com>
2024-03-26 15:05:49 +0530

    completed header section

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -27,7 +27,7 @@ const Header = () => {
           <FaMoon />
         </Button>
         <Link to="/signin">
-          <Button gradientDuoTone='purpleToBlue' className="">Sign In</Button>
+          <Button gradientDuoTone='purpleToBlue' outline>Sign In</Button>
         </Link>
         <Navbar.Toggle />
       </div>
