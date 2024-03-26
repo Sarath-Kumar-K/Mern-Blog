714c4d32c228fcd3892715a2f5a32699e121114e
SARATH KUMAR K <sarathkrishnan2332@gmail.com>
2024-03-26 12:32:02 +0530

    Installed flowbite-react

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,10 +6,12 @@ import Projects from "./pages/Projects";
 import Signup from "./pages/Signup";
 import Signin from "./pages/Signin";
 import Home from "./pages/Home";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <BrowserRouter>
+    <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About/>} />
