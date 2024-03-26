aa23972995a15ad4b7633430622324485349cb2d
SARATH KUMAR K <sarathkrishnan2332@gmail.com>
2024-03-26 12:51:11 +0530

    installed react-icons for icon usage

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Navbar } from "flowbite-react";
+import { Button, Navbar, TextInput } from "flowbite-react";
 import { Link } from "react-router-dom";
+import {AiOutlineSearch} from 'react-icons/ai'
 
 const Header = () => {
   return (
@@ -8,6 +9,17 @@ const Header = () => {
       <Link to="/" className="whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white">
         <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">Sarath's</span>Blog
       </Link>
+      <form>
+        <TextInput
+        type="text"
+        placeholder="serach"
+        rightIcon={AiOutlineSearch}
+        className="hidden lg:inline"
+        />
+      </form>
+      <Button className="w-12 h-10" color="gray" pill>
+        <AiOutlineSearch />
+      </Button>
     </Navbar>
   );
 };
