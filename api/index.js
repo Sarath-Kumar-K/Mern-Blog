91f1f1cb9821b8a14e5f4fdafbf2a46f63140ead
SARATH KUMAR K <sarathkrishnan2332@gmail.com>
2024-03-27 09:59:49 +0530

    Mongodb database Connected Successfully

--- api/index.js
+++ api/index.js
@@ -1,6 +1,16 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config();
 
 const app = express();
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log("Database connected successfully");
+}).catch((err) => {
+    console.log(err);
+});
 
 app.listen(3000, () => {
     console.log("Server is listening to the port 3000");
