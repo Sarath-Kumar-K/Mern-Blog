d45a0ea656bf624f5fee1ee9588ade666b94efa2
SARATH KUMAR K <sarathkrishnan2332@gmail.com>
2024-04-22 12:27:53 +0530

    added redux toolkit for global state management

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -2,24 +2,26 @@ import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { FcGoogle } from "react-icons/fc";
 import { Alert, Spinner } from "flowbite-react";
+import {useDispatch, useSelector} from 'react-redux';
+import { signInStart, signInSuccess, signInFailure } from "../redux/user/userSlice.js";
 
 const Signin = () => {
-  const [formaData, setFormData] = useState({});
-  const [errorMessage, setErrorMessage] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const [formaData,setFormData] = useState({});
+  // const [errorMessage, setErrorMessage] = useState(null);
+  // const [loading, setLoading] = useState(false);
+  const {loading, error:errorMessage} = useSelector(state => state.user);
+  const dispatch = useDispatch();
   const navigate = useNavigate();
   const handleOnchange = (e) => {
     setFormData({ ...formaData, [e.target.id]: e.target.value.trim() });
-    console.log(formaData);
   };
   const handleSubmit = async (e) => {
-    setErrorMessage(null);
     e.preventDefault();
-    if (!formaData.email || !formaData.password) {
-      return setErrorMessage("Please fill out all the details");
+    if (!formaData.email || !formaData.password || formaData.email === '' || formaData.password === '') {
+      return dispatch(signInFailure("Please fill out all the details"));
     }
     try {
-      setLoading(true);
+      dispatch(signInStart());
       const res = await fetch("/api/auth/signin", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
@@ -27,16 +29,14 @@ const Signin = () => {
       });
       const data = await res.json();
       if (data.success === false) {
-        setFormData(false);
-        return setErrorMessage(data.message);
+        dispatch(signInFailure(data.message));
       }
-      setLoading(false);
       if(res.ok){
+        dispatch(signInSuccess(data));
         navigate('/');
       }
     } catch (error) {
-      setErrorMessage(error.message);
-      setLoading(false);
+      dispatch(signInFailure(error.message));
     }
   };
   return (
