d11ab1ba2003193b19a8d3fc3940eb8ce26b3d31
SARATH KUMAR K <sarathkrishnan2332@gmail.com>
2024-05-14 17:19:16 +0530

    added dete user functionality to the admin in users section of dashboard

--- client/src/components/DashUsers.jsx
+++ client/src/components/DashUsers.jsx
@@ -154,7 +154,7 @@ const DashUsers = () => {
           <div className="absolute md:top-[30vh] md:left-[40vw]  m-5 z-20 rounded-md border-2 bg-white">
             <HiOutlineExclamationCircle className="my-5 mx-auto h-14 w-14 text-gray-400 dark:text-gray-200" />
             <h3 className="md:px-8 text-lg text-center mb-5 dark:text-gray-400 text-gray-500">
-              Are you sure you want to delete this user
+              Are you sure you want to delete this user?
             </h3>
             <div className="flex justify-center gap-5 mb-5">
               <Button color="failure" onClick={handleDeleteUser}>
