0ea1b83bb18d4cc1b1ebce716627f7708728607e
SARATH KUMAR K <sarathkrishnan2332@gmail.com>
2024-05-30 18:07:10 +0530

    added pre installation for deploying app

--- api/server.js
+++ api/server.js
@@ -6,9 +6,11 @@ import authRoutes from './routes/auth.route.js'
 import postRoutes from './routes/post.route.js'
 import commentRoutes from './routes/comment.route.js'
 import cookieParser from 'cookie-parser'
+import path from 'path';
 
 dotenv.config();
 
+const __dirname = path.resolve();
 const app = express();
 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -28,6 +30,12 @@ app.use('/api/user',userRoutes);
 app.use('/api/auth',authRoutes);
 app.use('/api/post',postRoutes);
 app.use('/api/comment',commentRoutes);
+
+app.use(express.static(path.join(__dirname,'/client/dist')));
+
+app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, 'client','dist', 'index.html'));
+});
 
 app.use((err,req,res,next) => {
     const statusCode = err.statusCode || 500;
